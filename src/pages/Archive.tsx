08217cc91459a42ef85694c159ee112cf74df289
Nuttapol Khumdang <nuttapol.chok@gmail.com>
2025-06-05 18:13:54 +0700

    fix: change heading title

--- src/pages/Archive.tsx
+++ src/pages/Archive.tsx
@@ -6,7 +6,7 @@ import NoteItem from "../components/Notes/NoteItem";
 const Archive: Component = () => {
   return (
     <main class="container mx-auto flex h-full max-w-2xl flex-col gap-4">
-      <header class="text-sm text-neutral-600">โน้ตอื่นๆ</header>
+      <header class="text-sm text-neutral-600">เก็บ</header>
       <section class="container grid grid-cols-3 gap-2">
         <For each={[0, 1, 2]}>
           {(container) => (
